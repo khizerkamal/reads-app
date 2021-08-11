aa9da066ae73a76404a5c5e7aa3d3da4d0d42f86
Muhammad Khizer Kamal <khizerkamal.03@gmail.com>
2021-08-11 13:33:41 +0500

    remove yarn

---
+++ build/precache-manifest.d1707ab85f8ba453d5ea17623ae5cd2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f266c4b9e6cc93d503e",
+    "url": "/reads-app/static/css/main.9c66a193.chunk.css"
+  },
+  {
+    "revision": "1f266c4b9e6cc93d503e",
+    "url": "/reads-app/static/js/main.1f266c4b.chunk.js"
+  },
+  {
+    "revision": "aeb38638971e4296845e",
+    "url": "/reads-app/static/js/1.aeb38638.chunk.js"
+  },
+  {
+    "revision": "6c275f45fcb1f7b68ae9",
+    "url": "/reads-app/static/js/runtime~main.6c275f45.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/reads-app/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/reads-app/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/reads-app/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "c4ffbda55778f3cf8f384bcafd0f84cf",
+    "url": "/reads-app/index.html"
+  }
+];
